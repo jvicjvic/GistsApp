022087f291cf7f678ddb23c3616f84d3fb2b70ea
João Victor Vital <jvital@gmail.com>
2024-08-27 14:51:04 -0300

    Initial Commit

---
+++ GistsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GistsApp
+//
+//  Created by jvic on 27/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
